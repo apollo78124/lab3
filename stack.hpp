8783a28d71f2107f8ed8d50538f4e3eeff769d95
apollo78124 <98apollo@naver.com>
2018-09-28 02:47:23 -0700

    2018/09/28 2:46am

--- stack.hpp
+++ stack.hpp
@@ -3,32 +3,55 @@
 
 #endif //LAB3_STACK_HPP
 
+// Store int data in a stack.
+// PRE : Stack is empty
+// POST : int values can be pushed and popped.
 class Stack {
         private:
             int top1;
             int a[];
             static constexpr int size = 10;
     public:
+
+        // Constructs the Stack.
+        // POST : Set the size of the stack to 10 and set the top index to -1
         Stack();
+
+        // Push the int to the stack
+        // PRE : Stack is either full or have space.
+        // POST : Increment the top index by 1 and store r into the top.
         bool push (int r);
+
+        // Delete a value from the top of the index.
+        // PRE : Stack can be empty or contain values.
+        // POST : Decrement top value by 1 which ignore the int value which was on the top.
         void pop();
 
+        // Return the top value.
+        // RETURN : Top index of the stack
         int top() const {
             return top1;
         }
 
+        // Return true if the stack is empty.
+        // PRE : Stack is either empty or not.
+        // POST : Same.
+        // RETURN : Boolean. True if the stack is empty, top value is -1
         bool empty() const {
-            if (top1 <= -1)
-                return true;
-            return false;
+            return top1 <= -1;
         }
 
+
+        // Return true if the stack is full.
+        // PRE : Stack can be either filled or not.
+        // POST : Same.
+        // RETURN : Boolean. True if the top index is 9.
         bool full() const {
-            if (top1 > 8)
-                return true;
-            return false;
+            return top1 > 8;
         }
 
+
+        // Prints all the values stored in the stack.
         void print() const {
             for (int i = 0; i < 10; i++) {
                 std::cout << a[i] << " ";
